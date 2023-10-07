eaca5e50be9a9c162a315a2ec53c30dca25bdf04
kobayashi <823913645@qq.com>
2023-10-07 23:02:18 +0800

    chore: remove webcontainer

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-import { WebContainer } from '@webcontainer/api'
-
-declare global {
-  interface Window {
-    __WEB_CONTAINER_SERVER_STARTED__?: boolean
-    __WEB_CONTAINER_INSTANCE__?: WebContainer
-  }
-}
